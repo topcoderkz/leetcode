e04c1ae5f4a1eef5ef67306f0aa858628c926ab2
Zumrat Kochshegulov님/AI Product Engineering팀 <1112267@sktelecom.com>
2019-04-14 22:27:26 +0900

    removed extra lines from problem B

--- 132/zumrat/b.py
+++ 132/zumrat/b.py
@@ -7,16 +7,13 @@
 
 class Solution:
     def maxAncestorDiff(self, root: TreeNode) -> int:
-        
         def calc(ancestors, elem):
             return max([abs(elem - ancestor) for ancestor in ancestors])
         
         def traverse(node, ancestors):
             if not node:
                 return
-            
             self.res = max(calc(ancestors, node.val), self.res)
-            
             traverse(node.left, ancestors + [node.val])
             traverse(node.right, ancestors + [node.val])
                 
